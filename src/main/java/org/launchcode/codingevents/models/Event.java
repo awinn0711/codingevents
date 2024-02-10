6abb5ca814c03af0cd583ac88c25802f90fef1af
Austin Winn <awinn0711@gmail.com>
2024-02-10 16:03:13 -0600

    created eventDetails class

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,15 +1,12 @@
 package org.launchcode.codingevents.models;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToOne;
+import jakarta.persistence.*;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import java.util.Objects;
-import jakarta.persistence.Entity;
 
 @Entity
 public class Event extends AbstractEntity {
@@ -18,22 +15,18 @@ public class Event extends AbstractEntity {
     @Size(min = 3, max = 50, message = "Name must be between 3 and 50 characters")
     private String name;
 
-    @Size(max = 50, message = "Description is too long")
-    private String description;
-
-    @NotBlank(message = "Email is required")
-    @Email(message = "Invalid Email")
-    private String contactEmail;
-
     @ManyToOne
     @NotNull(message = "Category is required")
     private EventCategory eventCategory;
 
+    @Valid
+    @NotNull
+    @OneToOne(cascade = CascadeType.ALL)
+    private EventDetails eventDetails;
+
     public Event() {};
-    public Event(String name, String description, String contactEmail, EventCategory eventCategory) {
+    public Event(String name, EventCategory eventCategory) {
         this.name = name;
-        this.description = description;
-        this.contactEmail = contactEmail;
         this.eventCategory = eventCategory;
     }
 
@@ -46,28 +39,20 @@ public class Event extends AbstractEntity {
         this.name = name;
     }
 
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getContactEmail() {
-        return contactEmail;
-    }
-
-    public void setContactEmail(String contactEmail) {
-        this.contactEmail = contactEmail;
-    }
-
     public EventCategory getEventCategory() {
         return eventCategory;
     }
 
     public void setEventCategory(EventCategory eventCategory) {
         this.eventCategory = eventCategory;
+    }
+
+    public EventDetails getEventDetails() {
+        return eventDetails;
+    }
+
+    public void setEventDetails(EventDetails eventDetails) {
+        this.eventDetails = eventDetails;
     }
 
     @Override
---
+++ src/main/java/org/launchcode/codingevents/models/EventDetails.java
@@ -0,0 +1,40 @@
+package org.launchcode.codingevents.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToOne;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public class EventDetails extends AbstractEntity{
+
+    @Size(max = 50, message = "Description is too long")
+    private String description;
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid Email")
+    private String contactEmail;
+
+    public EventDetails() {};
+    public EventDetails(String description, String contactEmail) {
+        this.description = description;
+        this.contactEmail = contactEmail;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
+    }
+}
