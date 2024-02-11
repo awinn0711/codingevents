426e154c560eb479f99e2743c626feb31a77ffb3
Austin Winn <awinn0711@gmail.com>
2024-02-11 09:43:39 -0600

    added functionality to add tags to events

--- src/main/java/org/launchcode/codingevents/models/Tag.java
+++ src/main/java/org/launchcode/codingevents/models/Tag.java
@@ -28,6 +28,10 @@ public class Tag extends AbstractEntity{
         return name;
     }
 
+    public String getDisplayName() {
+        return "#" + name + " ";
+    }
+
     public void setName(String name) {
         this.name = name;
     }
